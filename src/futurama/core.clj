cf97fd85cb9e8eb44a9cb2fb764819264dab0948
Jose Gomez <k13gomez@gmail.com>
2024-01-01 21:34:05 -0600

    feat: implemented initial macros and functions to support async workflow

--- src/futurama/core.clj
+++ src/futurama/core.clj
@@ -1,1 +1,165 @@
-(ns futurama.core)
+(ns futurama.core
+  (:require [clojure.core.async :refer [go <! <!! take!]]
+            [clojure.core.async.impl.protocols :as impl]
+            [clojure.core.async.impl.channels :refer [box]]
+            [clojure.core.async.impl.ioc-macros :as ioc])
+  (:import [clojure.lang Var]
+           [java.util.concurrent
+            CompletableFuture
+            ExecutorService
+            Future
+            ForkJoinPool]
+           [java.util.concurrent.locks Lock]
+           [java.util.function Function BiConsumer]))
+
+(def ^:dynamic *thread-pool* (ForkJoinPool/commonPool))
+
+(defmacro completable-future
+  "Asynchronously invokes the body inside a completable future, preserves the current thread binding frame,
+  using by default the `ForkJoinPool/commonPool`, the pool used can be specified via `*thread-pool*` binding."
+  ^CompletableFuture [& body]
+  `(let [binding-frame# (Var/cloneThreadBindingFrame) ;;; capture the thread local binding frame before start
+         ^CompletableFuture res-fut# (CompletableFuture.) ;;; this is the CompletableFuture being returned
+         ^ExecutorService pool# (or *thread-pool* (ForkJoinPool/commonPool))
+         ^Runnable fbody# (fn do-complete#
+                            []
+                            (try
+                              (Var/resetThreadBindingFrame binding-frame#) ;;; set the Clojure binding frame captured above
+                              (.complete res-fut# (do ~@body)) ;;; send the result of evaluating the body to the CompletableFuture
+                              (catch Exception ~'e
+                                (.completeExceptionally res-fut# ~'e)))) ;;; if we catch an exception we send it to the CompletableFuture
+         ^Future fut# (.submit pool# fbody#)
+         ^Function cancel# (reify Function
+                             (apply [~'_ ~'_]
+                               (future-cancel fut#)))] ;;; submit the work to the pool and get the FutureTask doing the work
+     ;;; if the CompletableFuture returns exceptionally
+     ;;; then cancel the Future which is currently doing the work
+     (.exceptionally res-fut# cancel#)
+     res-fut#))
+
+(extend-type CompletableFuture
+  impl/ReadPort
+  (take! [fut handler]
+    (let [^CompletableFuture fut fut
+          ^Lock handler handler
+          commit-handler (fn do-commit []
+                           (.lock handler)
+                           (let [take-cb (and (impl/active? handler) (impl/commit handler))]
+                             (.unlock handler)
+                             take-cb))]
+      (when-let [cb (commit-handler)]
+        (if (.isDone fut)
+          (let [val (try
+                      (.getNow fut nil)
+                      (catch Exception e
+                        e))]
+            (if (satisfies? impl/ReadPort val)
+              (do
+                (take! val (fn do-read
+                             [val]
+                             (if (satisfies? impl/ReadPort val)
+                               (take! val do-read)
+                               (cb val))))
+                nil)
+              (box val)))
+          (do
+            (.whenComplete ^CompletableFuture fut
+                           ^BiConsumer (reify BiConsumer
+                                         (accept [_ val ex]
+                                           (cond
+                                             (satisfies? impl/ReadPort val)
+                                             (take! val (fn do-read
+                                                          [val]
+                                                          (if (satisfies? impl/ReadPort val)
+                                                            (take! val do-read)
+                                                            (cb val))))
+
+                                             (some? val)
+                                             (cb val)
+
+                                             (some? ex)
+                                             (cb ex)
+
+                                             :else
+                                             (cb nil)))))
+            nil)))))
+  impl/WritePort
+  (put! [fut val handler]
+    (let [^CompletableFuture fut fut
+          ^Lock handler handler]
+      (when (nil? val)
+        (throw (IllegalArgumentException. "Can't put nil on channel")))
+      (if (.isDone fut)
+        (do
+          (.lock handler)
+          (when (impl/active? handler)
+            (impl/commit handler))
+          (.unlock handler)
+          (box false))
+        (do
+          (.lock handler)
+          (when (impl/active? handler)
+            (impl/commit handler))
+          (.unlock handler)
+          (box (.complete fut val))))))
+
+  impl/Channel
+  (close! [fut]
+    (.complete ^CompletableFuture fut nil))
+  (closed? [fut]
+    (.isDone ^CompletableFuture fut)))
+
+(defmacro async
+  "Asynchronously executes the body, returning immediately to the
+  calling thread. Additionally, any visible calls to !<!, <!, >! and alt!/alts!
+  channel operations within the body will block (if necessary) by
+  'parking' the calling thread rather than tying up an OS thread.
+  Upon completion of the operation, the body will be resumed.
+
+  async blocks should not (either directly or indirectly) perform operations
+  that may block indefinitely. Doing so risks depleting the fixed pool of
+  go block threads, causing all go block processing to stop. This includes
+  core.async blocking ops (those ending in !!) and other blocking IO.
+
+  Returns a channel which will receive the result of the body when
+  completed; the pool used can be specified via `*thread-pool*` binding."
+  [& body]
+  (let [crossing-env (zipmap (keys &env) (repeatedly gensym))]
+    `(let [c# (CompletableFuture.)
+           captured-bindings# (Var/getThreadBindingFrame)]
+       (.submit ^ExecutorService *thread-pool*
+                ^Runnable (^:once fn* []
+                                      (let [~@(mapcat (fn [[l sym]] [sym `(^:once fn* [] ~(vary-meta l dissoc :tag))]) crossing-env)
+                                            f# ~(ioc/state-machine `(try
+                                                                      ~@body
+                                                                      (catch Exception ~'e
+                                                                        ~'e)) 1 [crossing-env &env] ioc/async-custom-terminators)
+                                            state# (-> (f#)
+                                                       (ioc/aset-all! ioc/USER-START-IDX c#
+                                                                      ioc/BINDINGS-IDX captured-bindings#))]
+                                        (ioc/run-state-machine-wrapped state#))))
+       c#)))
+
+(defmacro !<!
+  "An improved macro version of <!, which also rethrows exceptions returned over the channel.
+  Must be called INSIDE a (go ...) or (async ...) block.
+  - Will return nil if closed.
+  - Will park if nothing is available.
+  - Will throw if an Exception is taken from port."
+  [v]
+  `(let [~'v (<! ~v)]
+     (if (instance? Exception ~'v)
+       (throw ~'v)
+       ~'v)))
+
+(defmacro !<!!
+  "An improved macro version of <!!, which also rethrows exceptions returned over the channel.
+  Must be called OUTSIDE a (go ...) or (async ...) block.
+  - Will return nil if closed.
+  - Will block if nothing is available.
+  - Will throw if a Exception is taken from port."
+  [v]
+  `(let [~'v (<!! ~v)]
+     (if (instance? Exception ~'v)
+       (throw ~'v)
+       ~'v)))
--- test/futurama/core_test.clj
+++ test/futurama/core_test.clj
@@ -1,1 +1,75 @@
-(ns futurama.core-test)
+(ns futurama.core-test
+  (:require [clojure.test :refer [deftest testing is]]
+            [futurama.core :refer [!<!! !<! async completable-future]]
+            [clojure.core.async :refer [go timeout put! take! close! chan <! >! <!! >!!] :as a])
+  (:import [java.util.concurrent CompletableFuture]
+           [clojure.lang ExceptionInfo]))
+
+(def ^:dynamic *test-val1* nil)
+(def test-val2 nil)
+
+(deftest async-ops
+  (testing "async put! test"
+    (let [^CompletableFuture f (CompletableFuture.)
+          v {:foo "bar"}]
+      (put! f v)
+      (is (= @f v))))
+  (testing "async take! test"
+    (let [^CompletableFuture f (CompletableFuture.)
+          v {:foo "bar"}
+          p (promise)]
+      (take! f (partial deliver p))
+      (.complete f v)
+      (is (= @p v))))
+  (testing "bindings test blocking - !<!!"
+    (binding [*test-val1* 100]
+      (with-redefs [test-val2 200]
+        (is (= [100 200]
+               (!<!!
+                 (async
+                   [*test-val1* test-val2]))
+               (!<!!
+                 (completable-future
+                   [*test-val1* test-val2])))))))
+  (testing "bindings test non-blocking - !<!"
+    (binding [*test-val1* 100]
+      (with-redefs [test-val2 200]
+        (<!!
+          (go
+            (is (= [100 200]
+                   (!<!
+                     (async
+                       [*test-val1* test-val2]))
+                   (!<!
+                     (completable-future
+                       [*test-val1* test-val2])))))))))
+  (testing "nested blocking take - !<!!"
+    (is (= (!<!! (async
+                   (go
+                     (CompletableFuture/completedFuture
+                       (completable-future
+                         (go
+                           (<! (timeout 50))
+                           {:foo "bar"}))))))
+           {:foo "bar"})))
+  (testing "nested non-blocking take - !<!"
+    (<!!
+      (async
+        (is (= (!<!! (async
+                       (go
+                         (CompletableFuture/completedFuture
+                           (completable-future
+                             (go
+                               (<! (timeout 50))
+                               {:foo "bar"}))))))
+               {:foo "bar"}))))))
+
+(deftest error-handling
+  (testing "throws async exception on take !<!"
+    (<!!
+      (async
+        (is (thrown-with-msg?
+              ExceptionInfo #"foobar"
+              (!<! (async
+                     (throw (ex-info "foobar" {}))
+                     ::result))))))))
